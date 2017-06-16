42a56dd3a0f97eacc5b3712787961a80603ef06b
company_shenghe <duoluoyaoye@sina.com>
2017-06-16 17:00:53 +0800

    update logging

--- Tools_package/log_mod.py
+++ Tools_package/log_mod.py
@@ -8,11 +8,19 @@ log mod
 
 import threading
 import logging
-
-
+from config.log_config import log_path
+from config.log_config import log_datefmt
+from config.log_config import log_format
+from config.log_config import log_level
+from config.log_config import log_filemode
 class LogMod:
 
     def tests(self):
+        logging.basicConfig(level=logging.DEBUG,
+                            format=log_format,
+                            datefmt=log_datefmt,
+                            filename=log_path,
+                            filemode=log_filemode)
 
         logging.warning("check check check")
 
--- config/log_config.py
+++ config/log_config.py
@@ -6,3 +6,29 @@
 logging mod
 """
 
+log_path = "../log/logs.txt"
+
+log_format = r"%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s : %(message)s"
+
+"""
+
+ %(levelno)s: 打印日志级别的数值
+ %(levelname)s: 打印日志级别名称
+ %(pathname)s: 打印当前执行程序的路径，其实就是sys.argv[0]
+ %(filename)s: 打印当前执行程序名
+ %(funcName)s: 打印日志的当前函数
+ %(lineno)d: 打印日志的当前行号
+ %(asctime)s: 打印日志的时间
+ %(thread)d: 打印线程ID
+ %(threadName)s: 打印线程名称
+ %(process)d: 打印进程ID
+ %(message)s: 打印日志信息
+
+"""
+
+
+log_level = "debug"
+
+log_datefmt = "%a, %d %b %Y %H:%M:%S"
+
+log_filemode = 'a'
